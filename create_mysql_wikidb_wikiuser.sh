151467f97668c71dd95318ff69be0a2a3f163537
Alexey Bogdanenko <abogdanenko@localhost.localdomain>
2014-08-04 14:25:32 +0400

    Create database wikidb, user wikiuser and give him access to the db

--- create_mysql_wikidb_wikiuser.sh
+++ create_mysql_wikidb_wikiuser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Adapted from https://github.com/tutumcloud/tutum-docker-lamp/raw/8ca614ad93e39274dc586e053287a14d5faff656/create_mysql_admin_user.sh
+
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
@@ -10,12 +12,13 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
+PASS=${MYSQL_WIKIUSER_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${MYSQL_WIKIUSER_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating MySQL admin user with ${_word} password"
 
-mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
+mysql -uroot -e "CREATE DATABASE wikidb"
+mysql -uroot -e "CREATE USER 'wikiuser'@'%' IDENTIFIED BY '$PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON wikidb.* TO 'wikiuser'@'%'"
 
 
 echo "=> Done!"
@@ -23,10 +26,8 @@ echo "=> Done!"
 echo "========================================================================"
 echo "You can now connect to this MySQL Server using:"
 echo ""
-echo "    mysql -uadmin -p$PASS -h<host> -P<port>"
+echo "    mysql -uwikiuser -p$PASS -h<host> -P<port>"
 echo ""
-echo "Please remember to change the above password as soon as possible!"
-echo "MySQL user 'root' has no password but only allows local connections"
 echo "========================================================================"
 
 mysqladmin -uroot shutdown

f3e16a3e8d5ed6520bd43e9a8627ec1eb7c1de74
Alexey Bogdanenko <abogdanenko@localhost.localdomain>
2014-08-11 10:10:36 +0400

    Put install. code inside bash functions

--- run1.sh
+++ run1.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-VOLUME_HOME="/var/lib/mysql"
+function check_install_mysql()
+{
+    local VOLUME_HOME="/var/lib/mysql"
 
-if [[ ! -d $VOLUME_HOME/mysql ]]; then
-    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
-    echo "=> Installing MySQL ..."
-    mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"  
-    /create_mysql_admin_user.sh
-    /create_mysql_wikidb_wikiuser.sh
-else
-    echo "=> Using an existing volume of MySQL"
-fi
+    if [[ ! -d $VOLUME_HOME/mysql ]]; then
+        echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+        echo "=> Installing MySQL ..."
+        mysql_install_db > /dev/null 2>&1
+        echo "=> Done!"
+        /create_mysql_admin_user.sh
+    else
+        echo "=> Using an existing volume of MySQL"
+    fi
+}
 
-WWW_HOME="/var/www"
-WIKI_FOLDER="wiki.dv.ru"
-WIKI_PATH="$WWW_HOME/$WIKI_FOLDER"
+function check_install_wiki()
+{
 
-if [[ ! -d $WIKI_PATH ]]; then
-    echo "=> Could not find $WIKI_FOLDER folder in $WWW_HOME"
-    echo "=> Installing MediaWiki ..."
-    mkdir -v $WIKI_PATH
-    tar -xf /downloads/mediawiki.tar.gz -C $WIKI_PATH --strip 1
-    chown --recursive www-data:www-data $WIKI_PATH
-    echo "=> Done!"
-else
-    echo "=> Using an existing folder $WIKI_PATH"
-fi
+    local WWW_HOME="/var/www"
+    local WIKI_FOLDER="wiki.dv.ru"
+    local WIKI_PATH="$WWW_HOME/$WIKI_FOLDER"
+
+    if [[ ! -d $WIKI_PATH ]]; then
+        echo "=> Could not find $WIKI_FOLDER folder in $WWW_HOME"
+        echo "=> Installing MediaWiki ..."
+        mkdir -v $WIKI_PATH
+        tar -xf /downloads/mediawiki.tar.gz -C $WIKI_PATH --strip 1
+        chown --recursive www-data:www-data $WIKI_PATH
+        echo "=> Extracted files to directory $WIKI_PATH"
+        /create_mysql_wikidb_wikiuser.sh
+    else
+        echo "=> Using an existing install of MediaWiki in folder $WIKI_PATH"
+  fi
+}
+
+check_install_mysql
+check_install_wiki
 
 exec supervisord -n
